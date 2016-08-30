4015b45c19bdb71ed5e70049998f1be6453e3763
Varas-C <Christian.varas.93@gmail.com>
2016-08-30 15:30:20 -0300

    Subo los Procesos Pokedex y las carpetas necesaria, esta todo vacio, es para trabajar aca

---
+++ Proc-Pokedex-Cliente/pokedex-cliente.c
@@ -0,0 +1,10 @@
+/*
+ * Pokedex-Cliente.c
+ *
+ *  Created on: 30/8/2016
+ *      Author: utnso
+ */
+
+#include <stdio.h>
+#include <commons/config.h>
+#include "headers/struct.h"
---
+++ Proc-Pokedex-Server/headers/struct.h
@@ -0,0 +1,13 @@
+/*
+ * struct.h
+ *
+ *  Created on: 30/8/2016
+ *      Author: utnso
+ */
+
+#ifndef HEADERS_STRUCT_H_
+#define HEADERS_STRUCT_H_
+
+
+
+#endif /* HEADERS_STRUCT_H_ */
