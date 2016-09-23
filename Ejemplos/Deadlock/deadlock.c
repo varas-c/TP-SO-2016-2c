329e8139a2578a1b3fd753fda9cdf27344c519d4
sebawars <sebawars@hotmail.com>
2016-09-22 22:11:15 -0300

    Pequeño avance de deadlock, ya lo terminare...

--- Ejemplos/Deadlock/deadlock.c
+++ Ejemplos/Deadlock/deadlock.c
@@ -164,15 +164,15 @@ int* generar_vector_recursos_disponibles(t_list* pokenests)
 
 void detectar_deadlock(int** matriz_peticiones_maximas, int** matriz_recursos_asignados,int* recursos_disponibles, int entrenadores, int pokenests)
 {
-	bool hay_deadlock=false;
-	int en_deadlock[entrenadores], pueden_ejecutar[entrenadores];
+	bool hay_deadlock=false, tiene_algun_recurso;
+	int en_deadlock[entrenadores], no_estan_en_deadlock[entrenadores];
 	int recursos_diferentes_disponibles; //PARA CONTROLAR QUÉ RECURSOS PUEDE CONSEGUIR UN ENTRENADOR DE LAS POKENESTS
 	int i,j;
 	int** matriz_necesidad = inicializar_matriz(entrenadores,pokenests);
 
     for(i=0;i<entrenadores;i++)
     {
-    	pueden_ejecutar[i]=0; //HASTA QUE NO VERIFIQUEMOS LOS RECURSOS QUE NECESITA CADA ENTRENADOR, ASUMIMOS QUE NO EJECUTA
+    	no_estan_en_deadlock[i]=0; //HASTA QUE NO VERIFIQUEMOS LOS RECURSOS QUE NECESITA CADA ENTRENADOR, ASUMIMOS QUE ESTA EN DL
     }
 
     /////SE CARGA MATRIZ DE NECESIDAD
@@ -186,18 +186,27 @@ void detectar_deadlock(int** matriz_peticiones_maximas, int** matriz_recursos_as
 	}
 
 	for(i=0;i<entrenadores;i++)  //RECORRO CADA ENTRENADOR
-	{
+	{	tiene_algun_recurso = false;
 		recursos_diferentes_disponibles = 0; //POR AHORA NO NECESITA
 		for(j=0;j<pokenests;j++)  //RECORRO CADA POKENEST (RECURSO)
 		{
-		    if( (!pueden_ejecutar[i])  &&  (matriz_necesidad[i][j] <= recursos_disponibles[j]) )  //VEMOS SI LA POKENEST (RECURSO) TIENE LAS INSTANCIAS QUE PIDE EL ENTRENADOR
+			if(matriz_recursos_asignados[i][j] > 0)
+			{
+				tiene_algun_recurso = true;
+			}
+		    if( (!no_estan_en_deadlock[i])  &&  (matriz_necesidad[i][j] <= recursos_disponibles[j]) )  //VEMOS SI LA POKENEST (RECURSO) TIENE LAS INSTANCIAS QUE PIDE EL ENTRENADOR
 		    {
 		    	recursos_diferentes_disponibles++; //ESTE ENTRENADOR PUEDE OBTENER LAS INSTANCIAS DE ESA POKENEST
-		    	if(recursos_diferentes_disponibles == pokenests) //PUEDE OBTENER TODOS LOS POKEMONES DE LAS POKENESTS
-		    	{
-		    		pueden_ejecutar[i] = 1; //ESE ENTRENADOR PUEDE EJECUTAR
-		    	}
+
 		    }
+		}
+    	if(recursos_diferentes_disponibles == pokenests) //PUEDE OBTENER TODOS LOS POKEMONES DE LAS POKENESTS
+    	{
+    		no_estan_en_deadlock[i] = 1; //ESE ENTRENADOR NO ESTA EN DL
+    	}
+		if((!tiene_algun_recurso) && (no_estan_en_deadlock[i] == 0))
+		{
+			no_estan_en_deadlock[i] = 1;    //NO TIENE RECURSO ASIGNADO, NO CUMPLE "ESPERA"
 		}
 	}
 
@@ -205,7 +214,7 @@ void detectar_deadlock(int** matriz_peticiones_maximas, int** matriz_recursos_as
 
 	for(i=0;i<entrenadores;i++)
 	{
-		if(!pueden_ejecutar[i])
+		if(!no_estan_en_deadlock[i])
 		{
 			hay_deadlock = true;
 			en_deadlock[j] = i; //GUARDO UN VECTOR CON EL NRO DE FILA DE LOS ENTRENADORES EN DEADLOCK
@@ -213,6 +222,47 @@ void detectar_deadlock(int** matriz_peticiones_maximas, int** matriz_recursos_as
 		}
 	}
 
+}
+
+int espera_circular(int recurso_esperado, int** matriz_asignados,int entrenadores)
+{
+
+}
+
+bool tiene_lo_que_pide(EntrenadorDL* entrenador, EntrenadorDL* otro_entrenador)
+{
+	int i,j;
+	Pokemon* pokeaux;
+	for(i=0;i<entrenador->cantidad_restante;i++)
+	{
+		for(j=0;j<list_size(otro_entrenador->recursos);j++)
+		{
+			if(entrenador->objetivos_restantes[i][0]==*(char*)list_get(otro_entrenador->recursos,j))
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+t_list* separar_deadlock(t_list* entrenadores)    //ME DEVUELVE UN DEADLOCK
+{
+	t_list* deadlock = list_create();
+	EntrenadorDL* aux,*primero;
+	int cantidad,i=0;
+	primero = ((EntrenadorDL*)list_get(entrenadores,0));//COMO TODOS ESTAN EN DEADLOCK, AGARRO UNO Y VEO CON CUALES ESTA EN DEADLOCK
+
+	for(i=1;i<cantidad;i++)
+	{
+		aux = (EntrenadorDL*)list_get(entrenadores,i);
+		if(tiene_lo_que_pide(primero,aux))
+		{
+			list_add(deadlock,aux);
+		}
+	}
+
+	return deadlock;
 
 }
 
