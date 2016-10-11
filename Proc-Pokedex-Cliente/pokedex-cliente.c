a0994bbabf0f18a4eb5ae36111b14dad39fe1d40
Matias-Rivas <matias_rivas@outlook.com>
2016-10-11 16:44:20 -0300

    Implemento PokeDex mediante sockets

--- Proc-Pokedex-Cliente/pokedex-cliente.c
+++ Proc-Pokedex-Cliente/pokedex-cliente.c
@@ -6,16 +6,139 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <fuse.h>
+#include <stddef.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#include <commons/string.h>
 #include <commons/config.h>
 #include "headers/struct.h"
 #include "headers/socket.h"
+#include "headers/osada.h"
+
+char* numero_IP = "127.0.0.1";
+char* numero_Puerto = "9995";
+int fd_server;
+
+void enviarCodigoyTamanio(int codigo, int tamanio)
+{
+	void* buffer;
+	buffer = malloc(2);
+	memset(buffer, codigo, 1);
+	memset(buffer+1, tamanio, 1);
+	send(fd_server, buffer, 2, 0);
+	free(buffer);
+	return;
+}
+
+static int osada_getattr(const char *path, struct stat *stbuf)
+{
+	void* buffer;
+	int res = 0;
+	int retorno = 0;
+
+	memset(stbuf, 0, sizeof(struct stat));
+
+	if (strcmp(path, "/") == 0)
+	{
+		stbuf->st_mode = S_IFDIR | 0777;
+		stbuf->st_nlink = 2;
+	}
+	else
+	{
+		enviarCodigoyTamanio(0, strlen(path)+1);
+		buffer = malloc(strlen(path)+1);
+		memcpy(buffer, path, strlen(path)+1);
+		res = send(fd_server, buffer, strlen(path)+1, 0);
+		free(buffer);
+		buffer = malloc(32);
+		if ((res = recv(fd_server, (osada_file*)buffer, 32, 0)) <= 0)
+		{
+			printf("El servidor se encuentra desconectado.\n");
+			retorno = -ENOENT;
+		}
+		else
+		{
+			osada_file* archivo = (osada_file*) buffer;
+			if ( archivo->state== DIRECTORY)
+			{
+				stbuf->st_mode = S_IFDIR | 0777;
+				stbuf->st_nlink = 2;
+			}
+			else if (archivo->state == REGULAR)
+			{
+				stbuf->st_mode = S_IFREG | 0777;
+				stbuf->st_nlink = 1;
+				stbuf->st_size = (__off_t)archivo->file_size;
+			}
+			else
+				retorno = -ENOENT;
+		}
+		free(buffer);
+	}
+
+	return retorno;
+}
+
+static int osada_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+	void* buffer;
+	int retorno = 0;
+	int res=0;
+
+	enviarCodigoyTamanio(1, strlen(path)+1);
+	buffer = malloc(strlen(path)+1);
+	memcpy(buffer, path, strlen(path)+1);
+	send(fd_server, buffer, strlen(path)+1, 0);
+	free(buffer);
+	buffer = malloc(1);
+	if ((res = recv(fd_server, buffer, 1, 0))<=0)
+	{
+		printf("El servidor se encuentra desconectado.\n");
+		retorno = -ENOENT;
+	}
+
+	if ((signed char) *(signed char*)buffer==1)
+	{
+		filler(buf, ".", NULL, 0);
+		filler(buf, "..", NULL, 0);
+		do
+		{
+			free(buffer);
+			buffer = malloc(17);
+			memset(buffer, 0, 17);
+			if ((res = recv(fd_server, buffer, 17, 0))<=0)
+			{
+				printf("El servidor se encuentra desconectado.\n");
+				retorno = -ENOENT;
+			}
+			filler(buf, buffer, NULL, 0);
+		}while(strlen(buffer)>0);
+	}
+	else
+		retorno = -ENOENT;
+
+	free(buffer);
+	return retorno;
+}
+
+
+static struct fuse_operations osada_oper = {
+		.getattr = osada_getattr,
+		.readdir = osada_readdir,
+};
+
 
 int main(int argc, char** argv)
 {
-	char* numero_IP = "127.0.0.1";
-	int numero_Puerto = 9995;
-	int fd_server = get_fdServer(numero_IP,numero_Puerto); //el fd_server es el "socket" que necesitas para comunicarte con el mapa
+	fd_server = get_fdServer(numero_IP,numero_Puerto); //el fd_server es el "socket" que necesitas para comunicarte con el mapa
 
+	return fuse_main(argc, argv, &osada_oper, NULL);
 
 	return 0;
 }
--- Proc-Pokedex-Server/pokedex-server.c
+++ Proc-Pokedex-Server/pokedex-server.c
@@ -7,12 +7,306 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+//#include <fuse.h>
 #include <commons/config.h>
 #include <commons/log.h>
+#include <commons/bitarray.h>
+#include <commons/string.h>
 #include "headers/struct.h"
 #include "headers/osada.h"
 #include "headers/socket.h"
 
+
+#define BLOQUE_NULO 0xFFFFFFFF
+#define DIRECTORIO_NULO 0xFFFF
+
+
+/* TAMAÑOS ESTRUCTURAS [BLOQUES]: (F= tamaño filesystem)
+ *
+ * HEADER = 1 (H)
+ * BITMAP = F / 512 (/8/64) (B)
+ * TABLA ARCHIVOS = 1024 (TAR)
+ * TABLA ASIGACIONES = (F - H - B - TAR) / 16 (TAS)
+ * DATOS = (F - H - B - TAR - TAS)
+ *
+ */
+
+//**** VARIABLES GLOBALES ****
+osada_header* header;
+t_bitarray bitmap;
+osada_file* tablaArchivos;
+osada_block_pointer* tablaAsignaciones;
+int tamanioAsig;
+osada_block* inicioDatos;
+int tamanioAdmin;
+
+
+//**** PROTOTIPOS ****
+int encontrarBloqueLibre();
+void marcarBloque(osada_block_pointer, uint8_t);
+void guardarBloque(osada_block, int);
+osada_block_pointer* obtenerPtroBloque(int);
+int buscarIndice(unsigned char*, int);
+int obtenerPrimerBloque(int);
+int obtenerBloqueSgte(int);
+int reservarNuevoBloque(int);
+int liberarBloque(int, uint8_t);
+void borrarArchivo(int);
+int* obtenerBloques(int, int*);
+int obtenerArchivo(char*);
+unsigned char* concatenarBloques(int*, int);
+int guardarDesdeBloque(int, void*, int, int);
+int actualizar(int, void*, int, int);
+
+
+
+void marcarBloque(osada_block_pointer bloque, uint8_t valor)
+{
+	if (valor) //Se realizan operaciones a nivel bit (bitwise) para distinguir bits dentro de los bytes del bitmap
+		//bitmap[(bloque)/8]= ((unsigned char)pow(2,(8-(bloque)%8)-1)) | (bitmap[(bloque)/8]);
+		bitarray_set_bit(&bitmap, bloque+tamanioAdmin);
+	else
+		//bitmap[(bloque)/8]= ~((unsigned char)pow(2,(8-(bloque)%8)-1)) & (bitmap[(bloque)/8]);
+		bitarray_clean_bit(&bitmap, bloque+tamanioAdmin);
+}
+
+void guardarBloque(osada_block dato, int bloque) //Podría agregarse la búsqueda de un bloque libre
+{
+	memset(inicioDatos[bloque], 0, OSADA_BLOCK_SIZE);
+	memcpy(inicioDatos[bloque], dato, OSADA_BLOCK_SIZE);
+	marcarBloque(bloque, 1);
+}
+
+osada_block_pointer* obtenerPtroBloque(int indice)
+{
+	return inicioDatos+indice;
+}
+
+int buscarIndice(unsigned char* archivo, int dirPadre) //Para ignorar dirPadre, asignarle un negativo.
+{
+	int i;
+	for (i=0;i<2048;i++)
+	{
+		if (!strcmp(tablaArchivos[i].fname, archivo)) //Recordar que strcmp devuelve 0 si son iguales
+			if (dirPadre < 0 || tablaArchivos[i].parent_directory == dirPadre)
+				return i;
+	}
+	return -1;
+}
+
+int obtenerPrimerBloque(int indice)
+{
+	if (indice<0 || indice>=2048)
+		return -1;
+	else
+		return tablaAsignaciones[tablaArchivos[indice].first_block];
+}
+
+int obtenerBloqueSgte(int bloqueActual)
+{
+	if (bloqueActual<0)
+		return -1;
+	else
+		return tablaAsignaciones[bloqueActual];
+}
+
+int liberarBloque(int bloque, uint8_t eliminarEnlace)
+{
+	if (bloque<0)
+		return -1;
+
+	if (eliminarEnlace)
+	{
+		int i;
+
+		for(i=0;i<header->data_blocks, i>=0;i++)
+		{
+			if (tablaAsignaciones[i]==bloque)
+			{
+				printf("Bloque vale: %d. El bloque que lo contiene es %d.\n\n", bloque, i);
+				tablaAsignaciones[i]=tablaAsignaciones[bloque];
+				i=-2;
+			}
+		}
+	}
+	marcarBloque(bloque, 0);
+
+	return 1;
+}
+
+void borrarArchivo(int indice)
+{
+	tablaArchivos[indice].state=DELETED;
+	int tamanio=0;
+	int* bloques=obtenerBloques(indice, &tamanio);
+	for(--tamanio;tamanio>0;tamanio--)
+		marcarBloque(bloques[tamanio], 0);
+}
+
+int* obtenerBloques(int indice, int* size) //Devuelve en orden los índices de los bloques que conforman el archivo.
+{
+	int* array;
+	int tamanio=0;
+	if (indice<0)
+	{
+		*size=-1;
+		return NULL;
+	}
+	else
+	{
+		if (tablaArchivos[indice].first_block==BLOQUE_NULO)
+		{
+			*size=0;
+			return NULL;
+		}
+		else
+		{
+			array=malloc(sizeof(int));
+			if (array==NULL) return NULL; //Verificacion de malloc
+			array[0]=tablaArchivos[indice].first_block;
+			int i = 0;
+			tamanio++;
+			while(tablaAsignaciones[array[i]]!=BLOQUE_NULO)
+			{
+				array=realloc(array, tamanio*sizeof(int)+sizeof(int));
+				if (array==NULL) //Verificacion de realloc
+				{
+					*size=-1;
+					return NULL;
+				}
+				tamanio++;
+				array[i+1]=tablaAsignaciones[array[i]];
+				i++;
+			}
+			(*size)=tamanio;
+			return array;
+		}
+	}
+}
+
+int obtenerArchivo(char* path)
+{
+	int i = 0;
+	while(path[i]!=0 && path[i]=='/')
+		i++;
+	if (i==strlen(path))
+		return-1;
+	else
+		i = 0;
+	char** directorios=NULL;
+	int dirActual=DIRECTORIO_NULO;
+	directorios = string_split(path, "/");
+
+	while(directorios[i]!=NULL)
+	{
+		dirActual = buscarIndice(directorios[i], dirActual);
+		if (dirActual<0)
+			return -1;
+		i++;
+	}
+
+	if (!i)
+		return -1;
+	else
+		return dirActual;
+}
+
+int encontrarBloqueLibre()
+{
+	int i;
+	for(i = tamanioAdmin; bitarray_test_bit(&bitmap, i) && i<header->fs_blocks;i++);
+
+	if (i<header->fs_blocks)
+		return i-tamanioAdmin;
+	else
+		return -1;
+}
+
+int reservarNuevoBloque(int bloqueAnterior)
+{
+	int nuevoBloque=encontrarBloqueLibre();
+	if (nuevoBloque<0)
+		return -1;
+
+	marcarBloque(nuevoBloque, 1);
+	if (bloqueAnterior>=0)
+		tablaAsignaciones[bloqueAnterior]=nuevoBloque;
+
+	tablaAsignaciones[nuevoBloque]=BLOQUE_NULO;
+	return nuevoBloque;
+}
+
+int guardarDesdeBloque(int bloqueActual, void* datos, int cantBytes, int offset)
+{
+	osada_block bloqueAux;
+	memcpy(bloqueAux, inicioDatos[bloqueActual], OSADA_BLOCK_SIZE);
+	int bytesCopiados= cantBytes>OSADA_BLOCK_SIZE ? OSADA_BLOCK_SIZE-offset : cantBytes;
+	memcpy(bloqueAux+offset, datos, bytesCopiados);
+	int bloqueSgte;
+
+	if (cantBytes!=bytesCopiados)
+	{
+		if ((bloqueSgte = obtenerBloqueSgte(bloqueActual))==BLOQUE_NULO)
+			if ((bloqueSgte = reservarNuevoBloque(bloqueActual))<0)
+				return 0;
+
+		if(!guardarDesdeBloque(bloqueSgte, datos+bytesCopiados, cantBytes-bytesCopiados, 0))
+			return 0;
+	}
+	guardarBloque(bloqueAux, bloqueActual);
+	return 1;
+}
+
+unsigned char* concatenarBloques(int* indicesBloques, int arraySize) //Hacer free de la variable a la que se asigna
+{
+	unsigned char* contenido;
+	int i;
+
+	contenido = malloc(OSADA_BLOCK_SIZE*arraySize);
+	memset(contenido, 0, arraySize*OSADA_BLOCK_SIZE);
+
+	for(i=0; i<arraySize;i++)
+		memcpy(contenido+(i*OSADA_BLOCK_SIZE), inicioDatos[indicesBloques[i]], OSADA_BLOCK_SIZE);
+
+	return contenido;
+}
+
+int actualizar(int archivo, void* nuevosDatos, int cantidadBytes, int offset)
+{
+	if (tablaArchivos[archivo].state != REGULAR || tablaArchivos[archivo].file_size < offset)
+		return 0;
+
+	int nroBloques = tablaArchivos[archivo].file_size / OSADA_BLOCK_SIZE + ((tablaArchivos[archivo].file_size % OSADA_BLOCK_SIZE)>0);
+	int bloqueInicial = 0;
+
+	if(nroBloques==0)
+	{
+		bloqueInicial = reservarNuevoBloque(-1);
+		if (bloqueInicial<0)
+			return 0;
+		tablaArchivos[archivo].first_block = bloqueInicial;
+	}
+
+	int* bloques = obtenerBloques(archivo, &bloqueInicial); //Uso bloqueInicial porque no me sirve la cantidad
+	bloqueInicial = nroBloques==0 ? 0 : offset/(nroBloques*OSADA_BLOCK_SIZE);
+
+
+	if (guardarDesdeBloque(bloques[bloqueInicial], nuevosDatos, cantidadBytes, offset))
+	{
+		if (cantidadBytes + offset > tablaArchivos[archivo].file_size)
+			tablaArchivos[archivo].file_size = cantidadBytes + offset;
+	}
+	else
+		return 0;
+
+	return 1;
+}
+
+/*
 leer_archivo_osada(FILE* f,osada_header* header, osada_file* file)
 {
 	//HEADER
@@ -37,31 +331,135 @@ leer_archivo_osada(FILE* f,osada_header* header, osada_file* file)
 	fread(file->file_size, 4, 1, f);
 	fread(file->lastmod, 4, 1, f);
 	fread(file->first_block, 4, 1, f);
+}
+*/
 
+void gestionarSocket(void* socket)
+{
+	int cliente = (int) socket;
+	int resultado = 0;
+	void* buffer;
+	uint8_t operacion;
+	uint8_t tamBuffer;
+	int archivo;
+	while(1)
+	{
+		buffer = malloc(2);
+		resultado= recv(cliente, buffer, 2, 0);
+		if (resultado<=0)
+			return;
+		memcpy(&operacion, buffer, 1);
+		memcpy(&tamBuffer, buffer+1, 1);
+		free(buffer);
+		buffer=malloc(tamBuffer);
+		switch(operacion)
+		{
+		case 0: if ((resultado = recv(cliente, buffer, tamBuffer, 0))<=0)
+					return;
+				archivo = obtenerArchivo((char*)buffer);
+				free(buffer);
+				if (archivo<0)
+				{
+					buffer = malloc(1);
+					signed char menosUno = -1;
+					memcpy(buffer, &menosUno, 1);
+					send(cliente, buffer, 1, 0);
+				}
+				else
+				{
+					buffer=malloc(sizeof(osada_file));
+					memcpy(buffer,tablaArchivos+archivo, sizeof(osada_file));
+					send(cliente, buffer, sizeof(osada_file), 0);
+				}
+				free(buffer);
+				break;
+
+		case 1: if ((resultado = recv(cliente, buffer, tamBuffer, 0))<=0)
+				{
+					printf("Cliente desconectado. Hilo cerrado\n\n");
+					return;
+				}
+				if (strcmp(buffer, "/") == 0)
+					archivo= DIRECTORIO_NULO;
+				else
+					archivo = obtenerArchivo((char*)buffer);
+				int i;
+				free(buffer);
+				if (archivo>=0)
+				{
+					uint8_t primero = 1;
+					for (i=0;i<2048;i++)
+					{
+						if (primero)
+						{
+							buffer = malloc(1);
+							primero = 0;
+							memset(buffer, 1, 1);
+							send(cliente, buffer, 1, 0);
+							free(buffer);
+						}
+						if (tablaArchivos[i].state != DELETED && tablaArchivos[i].parent_directory == archivo)
+							send(cliente, (void*)tablaArchivos[i].fname, 17, 0);
+					}
+					buffer = malloc(17);
+					memset(buffer, 0, 17);
+					send(cliente, buffer, 17, 0);
+				}
+				else
+				{
+					signed char menosUno = -1;
+					buffer=malloc(1);
+					memcpy(buffer, &menosUno, 1);
+					send(cliente, buffer, 1, 0);
+				}
+				free(buffer);
+				break;
+		}
+	}
+	return;
 }
 
 int main(int argc, char** argv)
 {
+	int fd_fileSystem = open("../challenge.bin", 2); //2 significa O_RDWR, leer y escribir
+	if (fd_fileSystem==-1)
+	{
+		printf("Archivo de file system no encontrado.\n");
+		exit(0);
+	}
+	struct stat fsStat;
+	fstat(fd_fileSystem, &fsStat);
 
-	/*
-	FILE* f;
-	osada_header* header;
-	osada_file* file;
+	header= mmap(0, fsStat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd_fileSystem, 0);
 
-	f= fopen ("ejemplo.bin", "rb+");
 
-	leer_archivo_osada(f,header,file);
-	*/
+	bitmap.size = header->bitmap_blocks * OSADA_BLOCK_SIZE;
+	bitmap.bitarray=header;
+	bitmap.bitarray+=OSADA_BLOCK_SIZE;
+	bitmap.mode=MSB_FIRST;
+
+	uint8_t* aux = header;
+	aux+= OSADA_BLOCK_SIZE + bitmap.size;
+	tablaArchivos = (osada_file*)aux;
+
+	aux+= 2048*sizeof(osada_file);
+	tablaAsignaciones = aux;
+	tamanioAsig = header->fs_blocks - header->allocations_table_offset - header->data_blocks; //En bloques
+
+	aux += tamanioAsig*OSADA_BLOCK_SIZE;
+	inicioDatos= aux;
+
+	tamanioAdmin = header->fs_blocks - header->data_blocks;
+
+//************************************************
 
 	fd_set master;   // conjunto maestro de descriptores de fichero
 	fd_set read_fds; // conjunto temporal de descriptores de fichero para select()
 
 	int fdmax;        // número máximo de descriptores de fichero
 	int listener;     // descriptor de socket a la escucha
-	void* buffer;
-	int tam_buffer = 200;
-	int nbytes;
-	int i, j;
+	int i;
+	pthread_t hilo;
 	FD_ZERO(&master);    // borra los conjuntos maestro y temporal
 	FD_ZERO(&read_fds);
 
@@ -75,51 +473,31 @@ int main(int argc, char** argv)
 	fdmax = listener; // por ahora es éste
 
 	// bucle principal
-		for (;;) {
-			read_fds = master; // cópialo
+		while(1)
+		{
+			read_fds = master;
 
 			//Buscamos los sockets que quieren realizar algo con Select
 			socket_select(fdmax, &read_fds);
 
 			// explorar conexiones existentes en busca de datos que leer
-			for(i = 0; i <= fdmax; i++) {
+			for(i = 0; i <= fdmax; i++)
+			{
 
-				if (FD_ISSET(i, &read_fds)) { // ¡¡tenemos datos!!
-
-					if (i == listener) {
+				if (FD_ISSET(i, &read_fds))
+				{
+					if (i == listener)
+					{
 						//ACEPTAMOS UN NUEVO CLIENTE
 						//socket_addNewConnection lo que ace
-						socket_addNewConection(listener,&master,&fdmax);
+						int cliente = socket_addNewConection(listener,&master,&fdmax);
+						int retval = pthread_create(&hilo, NULL, gestionarSocket, (void*) cliente);
+						printf("Nueva conexion detectada.Socket: %d\n", cliente);
+						//TODO revisar desconexion de cliente
 					}
-
-					else {
-
-						buffer = malloc(tam_buffer);
-						// gestionar datos de un cliente
-						if ((nbytes = recv(i, buffer,200, 0)) <= 0) { // error o conexión cerrada por el cliente
-
-							if (nbytes == 0) {
-								socket_closeConection(i,&master);
-
-							}
-							else {
-								perror("recv");
-							}
-
-						}
-
-						else
-						{
-							//El cliente "i" quiere algo
-
-
-						}
-					} // Esto es ¡TAN FEO!
 				}
 			}
 		}
 
-return 0;
-
-
+		return 0;
 }
